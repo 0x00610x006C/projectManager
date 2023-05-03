580da6887b6ed34857aca75bb353832a67a8dc0e
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-03 14:03:11 +0300

    created entity task

---
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/Task.java
@@ -0,0 +1,45 @@
+package i.m.allesssandro.projectmanager.projectManager;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Table(value = "tasks")
+public class Task
+{
+    @Id
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private TaskType type;
+
+    @Getter
+    @Setter
+    private TaskStatus status;
+
+    @Getter
+    @Setter
+    private LocalDateTime createdAt;
+
+    @Getter
+    @Setter
+    private LocalDateTime changedAt;
+
+    @Getter
+    @Setter
+    @Column("parent_project")
+    private Long parentProjectId;
+}
