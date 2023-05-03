4a32c2516f1b04a2192d7e42891eaafb9c39b8d0
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-03 14:04:23 +0300

    created entity for table projects

---
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/Project.java
@@ -0,0 +1,32 @@
+package i.m.allesssandro.projectmanager.projectManager;
+
+import i.m.allesssandro.projectmanager.auth.repo.Token;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@ToString
+@Table(value = "projects")
+public class Project
+{
+    @Id
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    @Column("parent_project")
+    private Long parentProjectId;
+}
