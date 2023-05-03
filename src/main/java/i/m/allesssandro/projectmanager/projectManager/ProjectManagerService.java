a2f5eff9f346dc833653f24d3479037e88de70b0
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-03 16:40:03 +0300

    created service layer for projects

--- src/main/java/i/m/allesssandro/projectmanager/projectManager/ProjectManagerService.java
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/ProjectManagerService.java
@@ -1,8 +1,23 @@
 package i.m.allesssandro.projectmanager.projectManager;
 
+import i.m.allesssandro.projectmanager.projectManager.repo.Project;
+import i.m.allesssandro.projectmanager.projectManager.repo.projectRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProjectManagerService
 {
+    private final projectRepository repository;
+
+    public ProjectManagerService(projectRepository repository)
+    {
+        this.repository = repository;
+    }
+
+    public List<Project> getAllProjects()
+    {
+        return repository.findAll();
+    }
 }
