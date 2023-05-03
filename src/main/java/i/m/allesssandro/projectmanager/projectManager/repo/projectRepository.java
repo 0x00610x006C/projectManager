dbc90ff43cc92b1e5c47dee9da5f4c03fe15d65c
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-03 16:38:41 +0300

    created repository for projects

---
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/repo/projectRepository.java
@@ -0,0 +1,15 @@
+package i.m.allesssandro.projectmanager.projectManager.repo;
+
+
+import i.m.allesssandro.projectmanager.projectManager.repo.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface projectRepository extends CrudRepository<Project, Long>
+{
+    @Override
+    List<Project> findAll();
+}
