6506a369d8be11726f616ed2b0c8d72e93b9e5ab
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-03 16:39:39 +0300

    created controller for requests with projects

--- src/main/java/i/m/allesssandro/projectmanager/projectManager/ProjectManagerController.java
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/ProjectManagerController.java
@@ -1,12 +1,32 @@
 package i.m.allesssandro.projectmanager.projectManager;
 
+import i.m.allesssandro.projectmanager.projectManager.repo.Project;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/user")
 public class ProjectManagerController
 {
+    private final ProjectManagerService projectManagerService;
 
+    public ProjectManagerController(ProjectManagerService projectManagerService)
+    {
+        this.projectManagerService = projectManagerService;
+    }
 
+    record GetAllProjectsResponse(
+            List<Project> projects
+    ){}
+
+    @GetMapping(value = "/projects")
+    public GetAllProjectsResponse getAllProjects()
+    {
+        List<Project> projects = projectManagerService.getAllProjects();
+
+        return new GetAllProjectsResponse(projects);
+    }
 }
