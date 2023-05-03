ef6ac62dece9694b917dce39fb9220d693adc4ed
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-03 14:02:36 +0300

    create controller for project manager logic

---
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/ProjectManagerController.java
@@ -0,0 +1,12 @@
+package i.m.allesssandro.projectmanager.projectManager;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/user")
+public class ProjectManagerController
+{
+
+
+}
