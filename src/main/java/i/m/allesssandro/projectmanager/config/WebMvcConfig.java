52d0c3a74863eeb0582d6e92b72554fa4a4902a8
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-05 00:45:16 +0300

    added points /api/user/** to authorization check

--- src/main/java/i/m/allesssandro/projectmanager/config/WebMvcConfig.java
+++ src/main/java/i/m/allesssandro/projectmanager/config/WebMvcConfig.java
@@ -18,6 +18,6 @@ public class WebMvcConfig implements WebMvcConfigurer
     public void addInterceptors(InterceptorRegistry registry)
     {
         registry.addInterceptor(autorizationInterceptor)
-                .addPathPatterns("/api/user/*");
+                .addPathPatterns("/api/user/**");
     }
 }
