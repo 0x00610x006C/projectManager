e9a62a8a0df142442455213cc763a8ad2e87c495
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-05 00:42:01 +0300

    created points /drop, /create, /edit

--- src/main/java/i/m/allesssandro/projectmanager/projectManager/ProjectManagerController.java
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/ProjectManagerController.java
@@ -1,14 +1,16 @@
 package i.m.allesssandro.projectmanager.projectManager;
 
+import i.m.allesssandro.projectmanager.auth.repo.User;
+import i.m.allesssandro.projectmanager.auth.repo.UserRole;
+import i.m.allesssandro.projectmanager.projectManager.exceptions.RoleHasNoPrivelege;
 import i.m.allesssandro.projectmanager.projectManager.repo.Project;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/user")
+@RequestMapping(value = "/api/user/projects")
 public class ProjectManagerController
 {
     private final ProjectManagerService projectManagerService;
@@ -22,11 +24,88 @@ public class ProjectManagerController
             List<Project> projects
     ){}
 
-    @GetMapping(value = "/projects")
+    @GetMapping(value = "/all")
     public GetAllProjectsResponse getAllProjects()
     {
         List<Project> projects = projectManagerService.getAllProjects();
 
         return new GetAllProjectsResponse(projects);
     }
+
+    record CreateProjectRequest (
+        String name,
+        Long parentId
+    ) {}
+
+    record CreateProjectResponse(
+            Project project
+    ) {}
+
+    @PostMapping(value = "/new")
+    public CreateProjectResponse createProject(HttpServletRequest request, @RequestBody CreateProjectRequest createProjectRequest)
+    {
+        User user = (User) request.getAttribute("user");
+
+        if (!UserRole.ADMIN.equals(user.getRole()))
+        {
+            throw new RoleHasNoPrivelege();
+        }
+
+        Project project = projectManagerService.create(
+                createProjectRequest.name(),
+                createProjectRequest.parentId()
+        );
+
+        return new CreateProjectResponse(project);
+    }
+
+    record DeleteProjectRequest (
+            Long id
+    ) {}
+
+    record DeleteProjectResponse (
+            String deletedIndexes
+    ) {}
+
+    @DeleteMapping(value = "/drop")
+    public DeleteProjectResponse deleteProject(HttpServletRequest request, @RequestBody DeleteProjectRequest deleteProjectRequest)
+    {
+        User user = (User) request.getAttribute("user");
+
+        if (!UserRole.ADMIN.equals(user.getRole()))
+        {
+            throw new RoleHasNoPrivelege();
+        }
+
+        String deletedIndexes = projectManagerService.dropProject(deleteProjectRequest.id());
+
+        return new DeleteProjectResponse(deletedIndexes);
+    }
+
+    record EditProjectRequest(
+            Long id,
+            String name
+    ) {}
+
+    record EditProjectResponse (
+            Project project
+    ) {}
+
+    @PatchMapping(value = "/edit")
+    public EditProjectResponse editProject(HttpServletRequest request, @RequestBody EditProjectRequest editProjectRequest)
+    {
+        User user = (User) request.getAttribute("user");
+
+        if (!UserRole.ADMIN.equals(user.getRole()))
+        {
+            throw new RoleHasNoPrivelege();
+        }
+
+        Project project = projectManagerService.editProject(editProjectRequest.id, editProjectRequest.name());
+
+        return new EditProjectResponse(project);
+    }
+
+
+
 }
