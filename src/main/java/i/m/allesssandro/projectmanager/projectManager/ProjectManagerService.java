a0f0cf2f8e6a924d27a57239c3633599df95ca86
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-05 00:42:34 +0300

    created created service methods for /drop, /create, /edit in controller

--- src/main/java/i/m/allesssandro/projectmanager/projectManager/ProjectManagerService.java
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/ProjectManagerService.java
@@ -1,5 +1,6 @@
 package i.m.allesssandro.projectmanager.projectManager;
 
+import i.m.allesssandro.projectmanager.projectManager.exceptions.ProjectNotExist;
 import i.m.allesssandro.projectmanager.projectManager.repo.Project;
 import i.m.allesssandro.projectmanager.projectManager.repo.projectRepository;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,37 @@ public class ProjectManagerService
     public List<Project> getAllProjects()
     {
         return repository.findAll();
+    }
+
+    public Project create(String name, Long parentId)
+    {
+        return repository.save(Project.of(name, parentId));
+    }
+
+    public String dropProject(Long id)
+    {
+        List<Project> children = repository.findAllByParentProjectId(id);
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Project project : children)
+        {
+            long childrenId = project.getId();
+            stringBuilder.append(dropProject(childrenId));
+            stringBuilder.append(childrenId).append(", ");
+        }
+
+        repository.deleteById(id);
+
+        return stringBuilder.toString();
+    }
+
+    public Project editProject(Long id, String name)
+    {
+        Project project = repository.findById(id).orElseThrow(ProjectNotExist::new);
+
+        project.setName(name);
+
+        return repository.save(project);
     }
 }
