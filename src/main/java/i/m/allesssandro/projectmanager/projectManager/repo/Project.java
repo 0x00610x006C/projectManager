8f1729afe1f1a962a2d49173e7735a39a062da34
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-05 00:41:12 +0300

    added constructors

--- src/main/java/i/m/allesssandro/projectmanager/projectManager/repo/Project.java
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/repo/Project.java
@@ -1,16 +1,12 @@
 package i.m.allesssandro.projectmanager.projectManager.repo;
 
-import i.m.allesssandro.projectmanager.auth.repo.Token;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.relational.core.mapping.Column;
-import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @ToString
 @Table(value = "projects")
@@ -29,4 +25,18 @@ public class Project
     @Setter
     @Column("parent_project")
     private Long parentProjectId;
+
+    public static Project of(String name, Long parentProjectId)
+    {
+        return new Project(null, name, parentProjectId);
+    }
+
+    @PersistenceConstructor
+    public Project(Long id, String name, Long parentProjectId)
+    {
+        this.id = id;
+        this.name = name;
+        this.parentProjectId = parentProjectId;
+    }
+
 }
