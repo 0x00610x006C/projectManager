cb00b4a33567730ebd5d81a0ade9a74978d85fd4
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-05 14:27:01 +0300

    create service for tasks, add method create for /new point in controller

---
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/tasks/TaskService.java
@@ -0,0 +1,30 @@
+package i.m.allesssandro.projectmanager.projectManager.tasks;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class TaskService
+{
+    private final TaskRepository repository;
+
+    public TaskService(TaskRepository repository)
+    {
+        this.repository = repository;
+    }
+
+
+    public Task create(String name, String type, Long projectId, Long authorId)
+    {
+        Task task = Task.of(name,
+                TaskType.valueOf(type),
+                TaskStatus.NEW,
+                LocalDateTime.now(),
+                LocalDateTime.now(),
+                authorId,
+                projectId);
+
+        return repository.save(task);
+    }
+}
