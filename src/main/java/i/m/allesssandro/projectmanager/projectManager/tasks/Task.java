3bc1f33bda5d2ebf8aa3748fa31c7dfba7f31649
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-05 14:27:39 +0300

    added author and constructor

--- src/main/java/i/m/allesssandro/projectmanager/projectManager/tasks/Task.java
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/tasks/Task.java
@@ -1,9 +1,11 @@
 package i.m.allesssandro.projectmanager.projectManager.tasks;
 
+import i.m.allesssandro.projectmanager.projectManager.projects.Project;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -40,6 +42,41 @@ public class Task
 
     @Getter
     @Setter
+    private Long author;
+
+    @Getter
+    @Setter
     @Column("parent_project")
     private Long parentProjectId;
+
+    public static Task of(String name,
+                          TaskType type,
+                          TaskStatus status,
+                          LocalDateTime createdAt,
+                          LocalDateTime changedAt,
+                          Long author,
+                          Long parentProjectId)
+    {
+        return new Task(null, name, type, status, createdAt, changedAt, author, parentProjectId);
+    }
+
+    @PersistenceConstructor
+    public Task(Long id,
+                String name,
+                TaskType type,
+                TaskStatus status,
+                LocalDateTime createdAt,
+                LocalDateTime changedAt,
+                Long author,
+                Long parentProjectId)
+    {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.changedAt = changedAt;
+        this.author = author;
+        this.parentProjectId = parentProjectId;
+    }
 }
