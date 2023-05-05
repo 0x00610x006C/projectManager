a3eafb3c169238fdc48f584d236597a822865def
0x00610x006C <i.m.allesssandro@gmail.com>
2023-05-05 16:06:33 +0300

    renamed

--- src/main/java/i/m/allesssandro/projectmanager/projectManager/tasks/errors/IncorrectTaskField.java
+++ src/main/java/i/m/allesssandro/projectmanager/projectManager/tasks/errors/IncorrectTaskField.java
@@ -3,11 +3,11 @@ package i.m.allesssandro.projectmanager.projectManager.tasks.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-public class IncorrectTaskType extends ResponseStatusException
+public class IncorrectTaskField extends ResponseStatusException
 {
-    public IncorrectTaskType()
+    public IncorrectTaskField(String value)
     {
-        super(HttpStatus.BAD_REQUEST, "incorrect task type");
+        super(HttpStatus.BAD_REQUEST, "incorrect value " + value + " in task field");
     }
 }
 
